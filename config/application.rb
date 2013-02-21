488b3fab3221be3918717c88f13b73a510a8e941
Derek <derek.a.rockwell@gmail.com>
2013-02-21 16:16:19 -0600

    heroku specific redis store configuration

--- config/application.rb
+++ config/application.rb
@@ -84,7 +84,13 @@ module Discourse
 
     # Use redis for our cache
     redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-    redis_store = ActiveSupport::Cache::RedisStore.new "redis://#{redis_config['host']}:#{redis_config['port']}/#{redis_config['cache_db']}"
+    
+    #redis_store for development leveraging local install
+    redis_store = ActiveSupport::Cache::RedisStore.new "redis://#{redis_config['host']}:#{redis_config['port']}/#{redis_config['cache_db']}" if Rails.env == "development"
+
+    #redis_store for production leveraging redistogo
+    redis_store = ActiveSupport::Cache::RedisStore.new "redis://redistogo:#{redis_config['password']}@#{redis_config['host']}:#{redis_config['port']}" if Rails.env == "production"
+
     redis_store.options[:namespace] = -> { DiscourseRedis.namespace }
     config.cache_store = redis_store
 
